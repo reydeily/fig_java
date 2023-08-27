f927a970d84aa4c29e03a9f57e4235bd3aee1836
reydeily <andreande963@gmail.com>
2023-08-27 15:35:37 -0300

    fim

--- src/Pages/Produto/index.tsx
+++ src/Pages/Produto/index.tsx
@@ -2,6 +2,9 @@ import * as S from "./styles"
 import inversor from "../../assets/inversor.jpg"
 import in1 from "../../assets/inversor-8k_3__1.jpg"
 import in2 from "../../assets/inversor-3k_2.jpg"
+import p1 from "../../assets/image 9.png"
+import p2 from "../../assets/image 10.png"
+import p3 from "../../assets/image 11.png"
 
 
 export function Produto() {
@@ -15,6 +18,12 @@ export function Produto() {
                 <a href="/produto">
                 <img src={in1} alt="8k" />
                 </a>
+                <h1>Veja também:</h1>
+                <a href="/produto">
+                <picture>
+                 <img src={p1} alt="p1" />
+                </picture>
+                </a>
             </S.Section>
             <S.Section>
                 <a href="/produto">
@@ -22,6 +31,11 @@ export function Produto() {
                 </a>
                 <a href="/produto">
                 <img src={inversor} alt="5k" />
+                </a>
+                <a href="/produto">
+                <picture>
+                 <img src={p2} alt="p1" />
+                </picture>
                 </a>
             </S.Section>
             <S.Section>
@@ -31,9 +45,12 @@ export function Produto() {
             <a href="/produto">
                 <img src={in2} alt="3k" />
             </a>
+            <a href="/produto">
+                <picture>
+                 <img src={p3} alt="p1" />
+                </picture>
+                </a>
             </S.Section>
-
-
         </>
     )
 }
--- src/Pages/Produto/styles.ts
+++ src/Pages/Produto/styles.ts
@@ -2,15 +2,24 @@ import styled from "styled-components"
 import { colors } from "../../styles/GlobalStyle"
 
 export const Section = styled.section`
-padding: 8rem;
-
+padding: 1rem;
+margin-right: 30rem;
+margin-left: 2rem;
 h2{
   display: flex;
-  padding-top: 0,5rem;
+  padding-top: 0rem;
   justify-content: center;
-  font-size: xx-large;
+  font-size: x-large;
   color: ${colors.preto}
-
+}
+h1{
+  display: flex;
+  margin-top: 3.5rem;
+  justify-content: center;
+  border: 3px solid ${colors.vclaro};
+  font-size: xx-large;
+  padding: 0.5rem;
+  color: ${colors.preto}
 }
 a{
   text-decoration: none;
@@ -20,7 +29,17 @@ a:hover {
   color: ${colors.preto}
 }
 
-
+picture {
+    margin-left: 1px;
+    padding: 1rem;
+    img {
+      margin-top: 3.5rem;
+      height: 18rem;
+      border: 3px solid ${colors.branco};
+      background-color: ${colors.cinza};
+      border-radius: 200px;
+    }
+  }
 img{
   padding-left: 4rem;
   width: 300px;
@@ -29,10 +48,27 @@ img{
   padding: 0.5rem;
   margin: 3px;
   align-items: center;
-  border-radius: 160px;
+  border-radius: 125px;
 }
 
   background-color: ${colors.branco};
   align-items: center;
   justify-content: center;
 `
+
+/*export const Section2 = styled.section`
+
+h2{
+  display: flex;
+
+  margin-right: 50rem;
+  padding-top: 25rem;
+  justify-content: left;
+  font-size: xx-large;
+  color: ${colors.preto}
+  
+}
+
+
+`
+*/
