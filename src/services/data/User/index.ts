16255399c638a6f911b93efacfebe464f7104ad0
reydeily <andrende963@gmail.com>
2023-11-17 08:35:36 -0300

    mod

---
+++ src/services/data/User/index.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://apiadonis2023.onrender.com',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  
+  export default api
